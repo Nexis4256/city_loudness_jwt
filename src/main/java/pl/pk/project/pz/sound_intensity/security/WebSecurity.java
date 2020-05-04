25c409cc07410540809a658ff09dc3201c3238cd
Nexis4256 <Nexis4256@gmail.com>
2020-05-04 19:11:56 +0200

    Adding permissions to users and console

--- src/main/java/pl/pk/project/pz/sound_intensity/security/WebSecurity.java
+++ src/main/java/pl/pk/project/pz/sound_intensity/security/WebSecurity.java
@@ -1,14 +1,18 @@
 package pl.pk.project.pz.sound_intensity.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static pl.pk.project.pz.sound_intensity.security.SecurityConstants.SIGN_UP_URL;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter
@@ -31,7 +35,18 @@ public class WebSecurity extends WebSecurityConfigurerAdapter
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        http.authorizeRequests().anyRequest().authenticated();
+        http.cors().and().csrf().disable().authorizeRequests()
+                .antMatchers(HttpMethod.POST,SIGN_UP_URL).permitAll()
+                .antMatchers("/users").permitAll()
+                .antMatchers("/console").permitAll()
+                .antMatchers("/console/**").permitAll()
+                .antMatchers("/users/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
+                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
+                // this disables session creation on Spring Security
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
